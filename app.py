0ddbc59d5a2c7a79f777f7a7e47332eec104eab7
Gowtham Easwar B <82211443+gowthameaswar@users.noreply.github.com>
2023-04-20 15:15:43 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,21 +1,22 @@
-import streamlit as st
 import pymongo
 from pymongo import MongoClient
+import streamlit as st
 import nltk
+import pandas as pd
+import plotly.express as px
+import matplotlib.pyplot as plt
 from nltk.sentiment.vader import SentimentIntensityAnalyzer
 
+
 # Download the VADER lexicon if you haven't done so before
-nltk.download('vader_lexicon')
-
-# Set up a client to connect to your MongoDB Atlas cluster
-client = MongoClient("mongodb+srv://gowtham07:gowtham07@awtcluster.aavwpkd.mongodb.net/socialchat")
-
-# Access the "socialchat" database and "posts" collection
-db = client["socialchat"]
-posts_collection = db["posts"]
+#nltk.download('vader_lexicon')
 
 # Initialize the VADER sentiment analyzer
 analyzer = SentimentIntensityAnalyzer()
+st.title('Sentiment Analysis for Chatgram')
+st.write("Negative-1")
+st.write("Neutral-2")
+st.write("Positive-3")
 
 # Define a function to analyze the sentiment of a given input text
 def analyze_sentiment(text):
@@ -23,11 +24,18 @@ def analyze_sentiment(text):
     scores = analyzer.polarity_scores(text)
     # Determine the sentiment label based on the compound score
     if scores['compound'] >= 0.05:
-        return 'Positive'
+        return 3
     elif scores['compound'] <= -0.05:
-        return 'Negative'
+        return 1
     else:
-        return 'Neutral'
+        return 2
+
+# Set up a client to connect to your MongoDB Atlas cluster
+client = MongoClient("mongodb+srv://gowtham07:gowtham07@awtcluster.aavwpkd.mongodb.net/socialchat")
+
+# Access the "socialchat" database and "posts" collection
+db = client["socialchat"]
+posts_collection = db["posts"]
 
 # Find all posts and retrieve the "title" and "content" fields
 posts = posts_collection.find({}, {"title": 1, "content": 1, "_id": 0})
@@ -41,24 +49,68 @@ for post in posts:
     titles.append(post["title"])
     contents.append(post["content"])
 
-# Loop through each title in the titles array and analyze its sentiment
+# Create empty arrays to store the sentiment labels
 t_sentiments = []
+c_sentiments = []
+
+# Loop through each title in the titles array and analyze its sentiment
 for title in titles:
     sentiment = analyze_sentiment(title)
     t_sentiments.append(sentiment)
 
 # Loop through each content in the contents array and analyze its sentiment
-c_sentiments = []
 for content in contents:
     sentiment = analyze_sentiment(content)
     c_sentiments.append(sentiment)
 
-# Create a Streamlit app to display the sentiment analysis results
-st.title("Sentiment Analysis Results")
-st.write("Titles:")
-for i in range(len(titles)):
-    st.write(f"{titles[i]}: {t_sentiments[i]}")
+# Display the title sentiments as a table
+st.write("Title Sentiments:")
+title_table = list(zip(titles, t_sentiments))
+st.table(title_table)
 
-st.write("Contents:")
-for i in range(len(contents)):
-    st.write(f"{contents[i]}: {c_sentiments[i]}")
+# Display the content sentiments as a table
+st.write("Content Sentiments:")
+content_table = list(zip(contents, c_sentiments))
+st.table(content_table)
+
+
+
+plt.figure(figsize=(8, 5))
+plt.hist(t_sentiments, bins=[1, 2, 3, 4], align='left')
+plt.xticks([1, 2, 3], ['Negative', 'Neutral', 'Positive'])
+plt.xlabel('Sentiment')
+plt.ylabel('Number of Titles')
+plt.title('Sentiment Analysis of Titles')
+st.pyplot(plt)
+
+# Plot the sentiment values for contents
+plt.figure(figsize=(8, 5))
+plt.hist(c_sentiments, bins=[1, 2, 3, 4], align='left')
+plt.xticks([1, 2, 3], ['Negative', 'Neutral', 'Positive'])
+plt.xlabel('Sentiment')
+plt.ylabel('Number of Contents')
+plt.title('Sentiment Analysis of Contents')
+st.pyplot(plt)
+
+df = pd.DataFrame({'Title Sentiments': t_sentiments, 'Content Sentiments': c_sentiments})
+
+# Create a line graph for Title Sentiments
+st.write('## Title Sentiments')
+fig1 = px.line(df, x=df.index, y='Title Sentiments')
+st.plotly_chart(fig1)
+
+# Create a line graph for Content Sentiments
+st.write('## Content Sentiments')
+fig2 = px.line(df, x=df.index, y='Content Sentiments')
+st.plotly_chart(fig2)
+
+fig, axs = plt.subplots(1, 2, figsize=(10, 5))
+axs[0].scatter(range(len(t_sentiments)), t_sentiments)
+axs[0].set_title('Title Sentiments')
+axs[0].set_xlabel('Post Index')
+axs[0].set_ylabel('Sentiment Value')
+axs[1].scatter(range(len(c_sentiments)), c_sentiments)
+axs[1].set_title('Content Sentiments')
+axs[1].set_xlabel('Post Index')
+axs[1].set_ylabel('Sentiment Value')
+st.pyplot(fig)
